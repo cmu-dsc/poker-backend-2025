0c1afb5818ba4de23d8f99059a73b9b818cba32a
Tobias Schamel <tobias.schamel@tum.de>
2024-03-23 18:39:42 -0400

    FIX: migrated away from ADC; properly using key frmo env

--- web-backend/src/server.ts
+++ web-backend/src/server.ts
@@ -5,8 +5,8 @@ import app from './app'
 import { BigQuery } from '@google-cloud/bigquery'
 import { Storage } from '@google-cloud/storage'
 
-export const bigqueryClient = new BigQuery({keyFile: env.GCLOUD_ADMIN_KEY, projectId: "pokerai-417521"})
-export const storageClient = new Storage({keyFile: env.GCLOUD_ADMIN_KEY, projectId: "pokerai-417521"});
+export const bigqueryClient = new BigQuery({credentials: JSON.parse(env.GCLOUD_ADMIN_KEY!), projectId: "pokerai-417521"})
+export const storageClient = new Storage({credentials: JSON.parse(env.GCLOUD_ADMIN_KEY!), projectId: "pokerai-417521"});
 
 const server = http.createServer(app)
 server.listen(env.PORT, () => {
